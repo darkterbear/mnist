7755cf6fa563a9f0c9a45466d6230d01bdb16f4b
Terrance Li <terranceli.terbear@gmail.com>
2018-10-27 12:02:01 -0700

    Consolidate backprop into ann

--- ann.js
+++ ann.js
@@ -1,7 +1,39 @@
 const fs = require('fs')
 const utils = require('./utils')
+const dS = utils.dSigmoid
+const dC = utils.dCost
 
-const backprop = require('./backprop')
+const backprop = (a, e, weights, biases, l, eta) => {
+	var w = weights[l - 1]
+	var b = biases[l - 1]
+
+	var rA = a[l]
+	var lA = a[l - 1]
+
+	w.forEach((wC, k) => {
+		wC.forEach((_, j) => {
+			const g = lA[k] * dS(rA[j]) * dC(rA[j], e[j])
+
+			w[k][j] -= g * eta
+		})
+	})
+
+	b.forEach((_, j) => {
+		const g = dS(rA[j]) * dC(rA[j], e[j])
+		b[j] -= g * eta
+	})
+
+	var nextA = lA.slice()
+	lA.forEach((_, k) => {
+		var g = 0
+		for (var j = 0; j < b.length; j++) {
+			g += w[k][j] * dS(rA[j]) * dC(rA[j], e[j])
+		}
+		nextA[k] -= g * eta
+	})
+
+	if (l > 1) backprop(a, nextA, weights, biases, l - 1, eta)
+}
 
 const feedForward = (w, b, i) => {
 	var a = [i]
